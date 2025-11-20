c919a1be55f1a9eddaf60f8193d675b997705817
yzhang0713 <yzhang0713@gmail.com>
2025-11-20 09:28:04 -0500

    Adding main program

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+    public:
+    A() {cout << "A" << endl;}
+};
+
+class B : public A {
+
+    public:
+    B() {cout << "B" << endl;}
+};
+
+int main() {
+
+    B* x = new B[2];
+    B* y = x[0];
+
+}
