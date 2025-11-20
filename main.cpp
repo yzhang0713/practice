b9542546b77c59f58b7bc42798fbec9f7f72f117
yzhang0713 <yzhang0713@gmail.com>
2025-11-20 09:52:04 -0500

    Adding hi method

--- main.cpp
+++ main.cpp
@@ -5,17 +5,25 @@ using namespace std;
 class A {
     public:
     A() {cout << "A" << endl;}
+    virtual void hi() {
+        cout << "hi" << endl;
+    }
 };
 
 class B : public A {
 
     public:
     B() {cout << "B" << endl;}
+    void hi() {
+        cout << "hi from B" << endl;
+    }
 };
 
 int main() {
 
     B* x = new B[2];
     B* y = &x[0];
+
+    y->hi();
 
 }
